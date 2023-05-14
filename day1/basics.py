c01882683878094a8a8a0010172120328f17590c
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2023-05-14 18:11:13 +0530

    Create basics.py

---
+++ day1/basics.py
@@ -0,0 +1,23 @@
+# # a="saurabh"
+# # b="gaurav"
+# # print(a+" "+b)
+# #
+# # c=20
+# # print(c+20)
+# #
+# #
+# #
+# a='2'
+# b='10'
+# c=100
+# d=200
+# print(a+b,c+d)
+
+a=10
+b=10
+print("addtion is :",a+b,"sub is :",a*b,a-b,a/b)
+
+# a="saurabh patil"
+# b="student at svkm IT"
+#
+# print(a+" "+b+"also this person does youtuber")
