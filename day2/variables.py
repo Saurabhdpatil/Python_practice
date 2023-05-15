f6f7834c5aa5efa0e8a5738ba99f6bb07e82e127
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2023-05-16 00:03:39 +0530

    Create variables.py

---
+++ day2/variables.py
@@ -0,0 +1,28 @@
+#code......
+a=10  #today i learn about variable, how to know there variable address......
+      #and also some pratice code.
+b=20
+print(a+b)
+print(id(a),id(b)) #this code gives you there memory location
+
+#code
+a=10
+b=20
+
+print('a') #if we put 'a' in single quotation then it work as string not a varible it will give you out a but if we put 'a' without any quotation then it will act as variable.
+
+#code (with quotation).
+
+a=20
+b=30
+
+print(a) #without single quotation.
+
+a=20
+b=40
+a=a+b
+
+print(a)
+
+
+
