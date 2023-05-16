abf2469cb47e9efe2f2515b6ecf0a9d721c43c7d
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2023-05-16 22:49:14 +0530

    Create stringconcatate.py

---
+++ day3/stringconcatate.py
@@ -0,0 +1,24 @@
+#Today learn about "string concatate" and there some examples.
+
+a="saurabh"
+b="gaurav"
+print(a+b) #here we add or that we can called it as a concatate of the two words may be its number of anything but , they belongs to same data type if it is not then that code will give you error
+ 
+#Error case
+a="saurabh"
+b=20
+
+print(a+b) #this code gives you error becoz of different type of two data type so here we can not concatate........ if you to concatate two string then they both belongs to same datatype.
+
+#if you want space between to two words.... then do like this 
+
+print(a+" "+b) #if you we use double quotation and give single space in between a and b......
+
+#differnt method to make space in between two strings.
+
+print(a,b) #just simply puting single comma in between a and b. its will work as to make space between two strings.
+
+#another pratice code.
+
+a=20
+print(a+30) #output is 50. #(we can also concatate like this.)
