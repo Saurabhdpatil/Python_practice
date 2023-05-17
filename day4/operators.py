6164fb58ae1338f7d2ae193266a495c2c9b0a7a6
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2023-05-18 00:17:23 +0530

    Create operators.py

---
+++ day4/operators.py
@@ -0,0 +1,16 @@
+#Arithmetic operators.
+
+#operators
+a=10
+b=20
+print(a+b) #addition operators.
+print(a-b) #substraction operators.
+print(a*b) #multiplication operators.
+print(a/b) #division operators.
+print(a**b) #Exponents operators.
+#This operators use for to find square root of any number like if you want to find square root of 5 i.e(5x5) but in this operators suppose if you want sqaure root of 5 then simply you have to use this operators by doing like this 5**2 (2 means multiplication of 5 twices times)
+
+print(a//b) #Floor Division operators.
+#actual ans is 0.5 but use this opertors its gives you integer value not a float.
+#this operators use for if you get out put in decimal but if you that float or decimal value in it integer then use this 'FLOOR DIVISION OPERATORS'
+#e.g if you divided 10 by 3 you'll get output in float like this 3.33333 something but after using this operators you'll get out put in integer value that is 3.
