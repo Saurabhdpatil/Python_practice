ca9c1daf2de7abbb4fbd5a9686fddd997e299651
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2023-05-19 00:06:50 +0530

    Update Assignment_operators.py

--- day5/Assignment_operators.py
+++ day5/Assignment_operators.py
@@ -6,8 +6,9 @@ x=5
 x=x+5
 print(x)
 
-# this operators are use to increment in value (+=)
 
+#this operators are use for increment.....
+# this operators are use to increment in value (+=)
 #Code....
 
 x=5
@@ -15,11 +16,27 @@ x+=4 #out-put is 9.
 print(x) #this increment operators are used to increment in value by some number which we put....
 
 #this operators are used to decrement...... this exact opposite of increment operators.
-
 #Code
 
 x=10
 x-=10
-print(x) #out-put is 0. 
-#and like this there are so many operators in that ww can use in our codes.......
+
+print(x) #out-put is 0.
+
+X=5
+X*=5
+print(x)
+
+x=5
+x/=5
+print(x)
+
+x=5
+x/=3
+print(x)
+
+
+
+
+
 
