4fb6029913e66a674970d8f7aa0e79b7cf75b4df
Saurabh Patil <82258961+Saurabhdpatil@users.noreply.github.com>
2023-05-18 23:51:13 +0530

    Create Assignment_operators.py

---
+++ day5/Assignment_operators.py
@@ -0,0 +1,25 @@
+# Today i Learn about Assignment_operators
+#so, what is Assignment operators , its is use to assigne value to in it.......
+
+#Code.... of equal to operators i.e(=)
+x=5
+x=x+5
+print(x)
+
+# this operators are use to increment in value (+=)
+
+#Code....
+
+x=5
+x+=4 #out-put is 9.
+print(x) #this increment operators are used to increment in value by some number which we put....
+
+#this operators are used to decrement...... this exact opposite of increment operators.
+
+#Code
+
+x=10
+x-=10
+print(x) #out-put is 0. 
+#and like this there are so many operators in that ww can use in our codes.......
+
